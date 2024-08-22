fd508b2852467b2cfe2b386bd35f5897369682c3
aritra-ganguly-03 <166842539+aritra-ganguly-03@users.noreply.github.com>
2024-08-22 19:17:26 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,8 +3,8 @@ import sys
 import numpy as np
 import pandas as pd
 
-from exception import CustomException
-from logger import logging
+from src.exception import CustomException
+from src.logger import logging
 
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
@@ -22,9 +22,21 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered Data Ingestion Component")
         try:
-            pass # Continue from here, stay tuned
+            df = pd.read_csv(os.path.join('notebooks', 'data', 'cinemaTicket_Ref.csv')) # Dataset Loading from the source
+            logging.info("Read Dataset")
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+            logging.info("Split Dataset into Train and Test")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False)
+            logging.info("Ingestion Complete")
 
 
         except Exception as e:
             logging.error("Error in Data Ingestion Component")
             raise CustomException(e, sys)
+        
+if __name__ == '__main__':
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
